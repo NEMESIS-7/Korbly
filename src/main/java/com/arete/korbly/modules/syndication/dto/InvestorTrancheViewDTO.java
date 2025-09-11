92dd0c461bbcd69c618b23cf2f0527664f796ebf
NEMESIS-7 <elikemfenuksu@gmail.com>
2025-09-11 15:24:17 +0000

    feat/multiple investors per tranche

--- src/main/java/com/arete/korbly/modules/syndication/dto/InvestorTrancheViewDTO.java
+++ src/main/java/com/arete/korbly/modules/syndication/dto/InvestorTrancheViewDTO.java
@@ -7,7 +7,8 @@ public record InvestorTrancheViewDTO(
         UUID trancheId,
         String trancheType,   // SENIOR, MEZZANINE, JUNIOR
         BigDecimal amount,    // total tranche size
+        BigDecimal allocatedSoFar,
+        BigDecimal remainingCapacity, // amount not yet allocated
         BigDecimal interestRate,
-        Integer tenorMonths,
-        BigDecimal remainingCapacity // amount not yet allocated
+        Integer tenorMonths
 ) {}
